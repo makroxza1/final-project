c5ebfddb22164f1ba5caa0e4119f92f8729db61e
makroxza1 <111824654+makroxza1@users.noreply.github.com>
2023-02-23 12:33:54 +0000

    Delete src directory

--- src/components/get_args_name.js
+++
@@ -1,9 +0,0 @@
-function getArgs(func) {
-    const funcStr = func.toString();
-    const paramStart = funcStr.indexOf('(') + 1;
-    const paramEnd = funcStr.indexOf(')');
-    const paramString = funcStr.substring(paramStart, paramEnd).trim();
-    return paramString.split(',').map(param => param.trim());
-}
-
-export default getArgs
