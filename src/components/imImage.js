0f91613abed52d884a94d1b154171cab9be5ec1c
makroxza1 <111824654+makroxza1@users.noreply.github.com>
2023-03-10 22:46:46 +0700

    Add files via upload

--- src/components/imImage.js
+++ src/components/imImage.js
@@ -1,8 +1,10 @@
-import imageUrl from './images/hello.png'
+import imageUrl from './images/334913374_2521665794649147_3446946612647888350_n.png'
+import cutandfill from './images/cutandfill.png'
 
 const images = () => {
     return [
-        {key:"123",img:imageUrl}
+        {key:"123",img:imageUrl},
+        {key:"123",img:cutandfill}
     ]
 }
 
